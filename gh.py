1cb7c963396832d34ec40a0b1506dc1cf0f33d49
shepherx <50464036+shepherx@users.noreply.github.com>
2022-02-24 09:34:23 -0700

    jango-tango-bango-mangoz-

---
+++ gh.py
@@ -0,0 +1,113 @@
+from random_word import RandomWords
+from numpy import random
+import sys
+import time
+
+italics = []
+italics.insert(0, ""
+               '\x1B[3m'
+               "")
+italics.insert(1, ""
+               '\x1B[0m'
+               "")
+thisshouldbeathing = "somehexcolorthingimnotgoingtogooglern"
+endendend = "alsosomeendtag"
+r = RandomWords()
+nastylisty = []
+bye = """
+xx-xx-xx-xx-xx-xx-xx-xx-xx
+   |\---/|
+   | ,_, |
+    \_`_/-..----.
+ ___/ `   ' ,""+ \  shepherx
+(__...'   __\    |`.___.';
+  (_,...'(_,.`__)/'.....+
+xx--xx--xx--xx--xx--xx--xx
+"""
+
+
+def kittys():
+    kittttt = f"""
+x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+                               __ _..._ _ 
+                           \ `)    `(/
+                           /` x   x \
+                           |         
+             .-"````"=-..--\=  xxx  /=
+           /`               `-.__=.'
+    _     / /\                 /o
+   ( \   / / |                 |
+    \ '-' /   >    /`""--.    /
+     '---'   /    ||      |   \\
+        shx  \___,,))      \_,,))  
+        zzz-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+        the-death-of-all-cats-falls-on-you |  
+        ------------------------------------
+        jklol - - - - write code for fun and
+        gitgud at it - - - fuck it off &
+        write programs til it hurts, even
+        if they make no sense. . . . . . .
+        x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+        @_shepherx on ig, evrywhelse shepherx
+        die for sleeeeeeep - - - - - - - - -
+        jk this is fun, print("hello world--
+        --insert curlys with awful yammering")
+        ---------------------------------------
+        
+    """
+    cat = input(
+        f"{italics[0]}what are you?\nhint: you're a fkn cat{italics[1]}\n")
+    fv = "meowwwwwwwwww!"
+    thelongestmeowofall = []
+    if cat == 'cat' or cat == 'yes' or cat == 'y':
+        for x in range(0, 999):
+            thelongestmeowofall.insert(x, fv)
+            print("""
+{italics[0]}please. . .  
+no more meows 
+- it hurts{italics[1]}""")
+            print(thelongestmeowofall)
+            print(f"""
+{italics[0]}fuck its fun to code{italics[1]}
+                  """)
+            print((f"""
+                {italics[0]} 
+---- you are a champion and a humwn being{endendend}
+honestly who has the time . . . . . .{italics[1]}
+                """))
+    for x in range(len(thelongestmeowofall)):
+        y = random.randint(1, 999)
+        print(thelongestmeowofall[x])
+        print(y)
+        print("""
+x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+here you go: some random fucking number to give your day some
+meaning, call a lib and ask if they have anything to fkn DO,
+whatever man . . . . . @_shepherx [shepherx on vh]on ig and 
+looking for crows and other weird birds, maybe bridges without 
+any water, write a script about it...x-x-x-x-x-x-x-x-x-x-x-x-x
+""")
+        print(bye)
+        print(f""" 
+love u :) 
+this script was legit so funny to write
+idk what the fuck
+enjoy programming
+it makes the world {italics[0]} go fast {italics[1]}
+write a script that makes no sense
+scream at birds
+fuck it
+im having a nice morning :)
+                        """)
+        print(bye)
+        time.sleep(5)
+        sys.exit()
+    else:
+        print("all the cats died. . . . .")
+        print("-x-x-x-x-x-x-x-x-x-x-")
+        print(kittttt)
+    sys.exit()
+
+
+kittys()
+# yuck
